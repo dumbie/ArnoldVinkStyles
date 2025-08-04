f05ff13603dd69f02d3323e50616f32cc5d60375
Arnold Vink <info@arnoldvink.com>
2025-08-04 21:03:56 +0200

    - Added MatchToVisibilityConverter (Used to match Enum to String in xaml).

---
+++ WinUI/Converters/MatchToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace ArnoldVinkStyles
+{
+    //Import:
+    //xmlns:AVStyles="using:ArnoldVinkStyles"
+    //Usage:
+    //Visibility="{Binding Enum, ConverterParameter='EnumAsString', Converter={AVStyles:MatchToVisibilityConverter}}"/>
+    public partial class MatchToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        protected override object ProvideValue()
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value.ToString() == parameter.ToString()) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
