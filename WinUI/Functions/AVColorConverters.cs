4bcc8201a2023f8af26d3f722c4925c477c5d7d9
Arnold Vink <info@arnoldvink.com>
2025-09-15 12:15:29 +0200

    - Added code to convert hex color string to SolidColorBrush.

---
+++ WinUI/Functions/AVColorConverters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace ArnoldVinkStyles
+{
+    public partial class AVColorConverters
+    {
+        /// <summary>
+        /// Convert hex color string to SolidColorBrush
+        /// Usage: HexToSolidColorBrush("#FF000000");
+        /// </summary>
+        public static SolidColorBrush HexToSolidColorBrush(string colorHex)
+        {
+            try
+            {
+                //Check if hex contains alpha channel
+                if (colorHex.Length == 9)
+                {
+                    return new SolidColorBrush(Color.FromArgb(Convert.ToByte(colorHex.Substring(1, 2), 16), Convert.ToByte(colorHex.Substring(3, 2), 16), Convert.ToByte(colorHex.Substring(5, 2), 16), Convert.ToByte(colorHex.Substring(7, 2), 16)));
+                }
+                else
+                {
+                    return new SolidColorBrush(Color.FromArgb(255, Convert.ToByte(colorHex.Substring(1, 2), 16), Convert.ToByte(colorHex.Substring(3, 2), 16), Convert.ToByte(colorHex.Substring(5, 2), 16)));
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
