099adc046df3651e594560ce12e8b4d1ce99e3c8
Arnold Vink <info@arnoldvink.com>
2026-01-02 16:59:59 +0100

    - Ported AVColors code to WinUI.

---
+++ WinUI/Styles/AVColors.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace ArnoldVinkStyles
+{
+    public partial class AVColors
+    {
+        //Adjust color brightness
+        public static SolidColorBrush AdjustColorBrightness(SolidColorBrush solidColorBrush, double brightness)
+        {
+            try
+            {
+                Color adjustedColor = Color.FromArgb(solidColorBrush.Color.A, (byte)(solidColorBrush.Color.R * brightness), (byte)(solidColorBrush.Color.G * brightness), (byte)(solidColorBrush.Color.B * brightness));
+                return new SolidColorBrush(adjustedColor);
+            }
+            catch { }
+            return solidColorBrush;
+        }
+
+        //Adjust color opacity
+        public static SolidColorBrush AdjustColorOpacity(SolidColorBrush solidColorBrush, double opacity)
+        {
+            try
+            {
+                Color adjustedColor = Color.FromArgb((byte)(solidColorBrush.Color.A * opacity), solidColorBrush.Color.R, solidColorBrush.Color.G, solidColorBrush.Color.B);
+                return new SolidColorBrush(adjustedColor);
+            }
+            catch { }
+            return solidColorBrush;
+        }
+    }
+}
